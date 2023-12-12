18ee8fe2fe3e652f1fa6d4c404931cd042cd27b8
Praveen Maharaj <praveenmaharaj.625@gmail.com>
2023-12-12 11:53:45 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,374 @@
+export const Bio = {
+  name: "Praveen Maharaj",
+  roles: [
+    "Full Stack Developer",
+    "competitive Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Maharaj7809",
+  resume:
+    "https://docs.google.com/document/d/1V2mkUR22Q32ioWckPQRD_oZcJ1mPqCDzJr5gSlqTe34/edit?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/maharaj7/",
+  twitter: "https://twitter.com/RishavChanda",
+  insta: "https://www.instagram.com/rishav_chanda/",
+  facebook: "https://www.facebook.com/rishav.chanda.165/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Material UI",
+        image: "",
+      },
+      {
+        name: "Flutter",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image: "",
+      },
+      {
+        name: "Graph Ql",
+        image: "https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+ 
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+      {
+        name: "Adobe XD",
+        image:
+          "https://camo.githubusercontent.com/c205ecbe12500177d102169d97bc1c17c545155fdf5ec78c08d54ac53e5b38c1/68747470733a2f2f63646e2e776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f61646f62652d78642e737667",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+
+  {
+    id: 0,
+    img: "https://i.ibb.co/0BfGDGC/images-1-nvcti.jpg",
+    role: "Full Stack Web Developer",
+    company: "NVCTI IIT Dhanbad",
+    date: "June 2022 - Present",
+    desc: "The development initiatives for NVCTI's digital infrastructure have been extensive and impactful. Commencing with the official website, I integrated MaterialUI to craft a seamless user experience, constructing the Homepage and an Authentication Page. For the Event Management Portal, I created an admin dashboard and a dynamic form akin to Google Forms. This streamlined workflow led to a notable reduction in processing time by 40%, empowering efficient event organization. The Inventory Management Portal underwent significant enhancements with added search functionality and the capability for lab mentors to upload inventory details via Excel sheets. This overhaul resulted in a 50% boost in productivity and an impressive 70% reduction in errors. Additionally, the introduction of an application form for the Product Development Internship (PDI) streamlined the application process, reducing processing time by 60%. Finally, the implementation of an Email Notification System using Nodemailer has greatly improved applicant engagement and satisfaction, leading to a 30% increase in overall applicant interactions. These developments collectively transformed NVCTI's digital landscape, optimizing operations and significantly enhancing user experiences.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+   
+  },
+
+  {
+    id: 1,
+    img: "https://i.ibb.co/kBJPnC9/kk-Lyg-Xmm-400x400-ACIC.jpg",
+    role: "Act2INNOVATE— Ideathon(ACIC–IIT(ISM))",
+    company: "IIT Dhanbad",
+    date: "June 2022 - Present",
+    desc: "This event nurtures and incubates a knack for creating something of value and promotes an innovation culture. Myteam is working on the idea of a healthcare application that tends to all age groups' healthcare and fitness needs. ",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+   
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/RPLjD85/download-HF.jpg",
+    role: "Hackfest 2K22 |IIT (ISM) DHANBAD",
+    company: "IIT Dhanbad",
+    date: "6th May 2022 - 8th May 2022 ",
+    desc: "Hackfest is a 36 hours hackathon organized by NVCTI IIT(ISM) DHANBAD.Companies like ATLASSIAN.CAPGEMINI, TEXMIN, FLAM, and many more sponsored this event and also gave their problem statements to work on. I participated with my team  of four members and chose the problem statement given by Atlassian company. 36hours of continuous zoom call, online meets, attending tech talks meanwhile, and working on your project, was really an awesome experience",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Docker",
+      "AWS",
+      "MongoDB",
+    ],
+  },
+  {
+    id: 3,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+    role: "Open Source Contributor ",
+    company: "GirlScript Summer of Code",
+    date: "May 2023 - Present",
+    desc: "Contributing to different open-source projects and learn from industry experts",
+    doc: "https://media.licdn.com/dms/image/D4D22AQGzSpgFaR4hsQ/feedshare-shrink_800/0/1684079632872?e=1687996800&v=beta&t=i0xWn_k32Oj9SxKfc6rARSbOghXD9Ge6WyEb2p2vvrc",
+  },
+  
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://i.ibb.co/8cPfjcw/download-iit-logo.jpg",
+    school: "Indian Institute of Technology (IIT) Dhanbad",
+    date: "NOV 2021 - May 2025",
+    grade: "8.13 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Mechanical Engineering at Indian Institute of  Technology, Dhanbad. I have completed 5 semesters and have a CGPA of 8.13. I have taken courses in Data Structures and Algorithms, Object-Oriented Programming, Database Management Systems, among others. I am also a member of the CodeISM and CodeElectronics at IIT Dhanbad , where I am learning and working on exciting projects with a team of talented developers.",
+    degree: "Bachelor of Technology - BTech, Mechanical Engineering",
+  },
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
+    school: "SS+2 High School , Bokaro",
+    date: "Apr 2019 - Apr 2021",
+    grade: "88.4%",
+    desc: "I completed my class 12 high school education at  SS+2 Hign School, Kasmar .",
+    degree: "ISC(XII), Science ",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/2NBTkZv/download-ljps-logo.jpg",
+    school: "Leela Janki Public School, Bokaro",
+    date: "Mar 2019 - Apr 2019",
+    grade: "95.4%",
+    desc: "I completed my class 10 education at Leela Janki Public School, Bokaro, where I studied Science with Computer Application.",
+    degree: "CBSE(X), ",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "ChatApp",
+    date: "Jun 2023 - Aug 2023",
+    description:
+      " The project is a real-time platform enabling group and personal chatting with integrated video call capabilities powered by WebRTC. It incorporates robust user authentication and authorization using the MERN stack. Additionally, an AI chatbot is seamlessly integrated to enhance user experience and interaction within the platform. ",
+    image:"https://i.ibb.co/sJGsLgj/Screenshot-2023-12-11-204703.png",
+    tags: [
+      "ReactJS",
+      "AWS",
+      "WebRTC",
+      "AI/ML",
+      "Socket.io",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+      "Redux",
+    ],
+    category: "web app",
+    github: "https://github.com/Maharaj7809/Chat-App",
+    webapp: "https://chat-app-lyart-sigma.vercel.app/",
+  },
+  {
+    id: 0,
+    title: "Admin Dashboard",
+    date: "Apr 2023 - May 2023",
+    description:
+      "The project is a feature-rich platform, offering an intuitive user interface and robust functionalities such as user management and data visualization. Real-time data updates, including geography location charts, enhance the dynamic user experience. It efficiently integrates Redux for streamlined state management, ensuring optimal performance and responsiveness. With its focus on intuitive design and powerful features, the platform delivers a seamless experience for users, facilitating easy navigation, comprehensive data representation, and real-time insights in a responsive and dynamic environment ",
+    image:
+      "https://i.ibb.co/r6KQmsG/Screenshot-2023-12-12-094627.png",
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux","Nivo Charts","MaterialUI","ThreeJs"],
+    category: "web app",
+    github: "https://github.com/Maharaj7809/Dashboard",
+    webapp: "https://dashboard-x1h2.onrender.com/",
+   
+  },
+  {
+    id: 1,
+    title: "Ecommerce App",
+    date: "Oct 2022 - Aug 2023",
+    description:
+      "The ecommerce website stands as a robust, scalable platform leveraging Strapi for efficient content and product data management. It hosts comprehensive features like product listing, cart functionality, and adept management of orders, transactions, and user authentication. Seamlessly integrated with Stripe payment gateways, it ensures secure and smooth transactions. This platform not only provides a streamlined shopping experience but also showcases the potential of utilizing Strapi for content and product data management, offering a secure, feature-rich ecommerce environment with intuitive functionalities.",
+    image:
+      "https://i.ibb.co/xF1FWGx/Screenshot-2023-12-12-094717.png",
+    tags: [
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Strapi",
+      "Redux",
+      "Stripe",
+    ],
+    category: "web app",
+    github: "https://github.com/Maharaj7809/Ecommerce_App",
+    webapp: "https://ecommerce-6300.onrender.com/",
+  },
+  {
+    id: 2,
+    title: "MERN Blog App",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "The project features an intuitive user interface with responsive layouts and robust admin authentication and authorization functionalities. It integrates a database system adept at storing and managing diverse content such as blog posts, user profiles, and comments. This cohesive system ensures a seamless user experience, adaptable design across devices, and secure access for administrators. By combining a user-centric interface with efficient database management, the platform delivers a versatile environment capable of handling dynamic content and user interactions effectively.",
+    image:
+      "https://i.ibb.co/G9wML5D/Screenshot-2023-12-12-094737.png",
+      tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux","Nivo Charts","MaterialUI","ThreeJs"],
+    category: "web app",
+    github: "https://github.com/Maharaj7809/Faverr_App",
+    webapp: "https://faverap.onrender.com/",
+  
+  },
+  {
+    id: 3,
+    title: "Travel Map Addisor",
+    date: "Dec 2022 - Apr 2023",
+    description:
+      "The project leverages Google Maps API to empower users with visual representations of travel destinations. By employing Geolocation API, it accurately pinpoints user locations and integrates Google Places API to showcase detailed information about hotels, restaurants, and attractions. Offering real-time updates, it ensures users have immediate access to nearby attractions, dining options, and accommodations. Seamlessly combining these APIs, the platform enriches travel experiences by providing comprehensive, up-to-date information, aiding users in planning and exploring destinations with ease, efficiency, and accuracy.",
+    image:
+      "https://i.ibb.co/rHVz6gc/Screenshot-2023-12-12-094801.png",
+      tags: ["React Js", "API","Nivo Charts","MaterialUI"],
+    category: "web app",
+    github: "https://github.com/Maharaj7809/Travel_Guide",
+    webapp: "https://travel-advissor-app.onrender.com/",
+  },
+  {
+    id: 10,
+    title: "Movie App",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "The Movie App, crafted with ReactJS and the MERN stack, stands as a dynamic platform leveraging APIs. It offers an immersive movie-watching experience by seamlessly integrating various services. Utilizing ReactJS for a responsive front-end, it connects to a MongoDB database through Node.js and Express for robust backend support. The incorporation of APIs enriches the app with extensive movie data, enabling users to browse, discover, and access comprehensive information about movies. Through its intuitive interface and powerful tech stack, this app redefines entertainment, providing a seamless platform for movie enthusiasts to explore, discover, and enjoy their favorite films.",
+    image:
+      "https://i.ibb.co/XSZxNHw/Screenshot-2023-12-12-094857.png",
+    tags: ["ReactJS", "API", "Axios","NodeJS","ThreeJS","3D Three-Fibre"],
+    category: "web app",
+    github: "https://github.com/Maharaj7809/Movie-App",
+    webapp: "https://movie-irx5.onrender.com/",
+  },
+  {
+    id: 8,
+    title: "Face Recognition",
+    date: "Jan 2021",
+    description:
+      "A Face recognition python app made with OpenCV. It uses face_recognition library to detect faces. It uses the webcam to detect faces. It also has a search bar to search for a particular face.",
+    image:
+      "https://i.ibb.co/FD0TYtp/face.jpg",
+    tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+    category: "machine learning",
+    github: "https://github.com/Maharaj7809/ML-Model",
+    webapp: "https://github.com/Maharaj7809/ML-Model",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2021, text: "Started my journey" },
+  { year: 2022, text: "Worked as a freelance developer" },
+  { year: 2021, text: "Founded JavaScript Mastery" },
+  { year: 2022, text: "Shared my projects with the world" },
+  { year: 2023, text: "Started my own platform" },
+];
